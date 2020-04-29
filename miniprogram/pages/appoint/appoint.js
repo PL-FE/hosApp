a60dd1a3f0390a4c3b21647ae5a1e7e1f2761860
yao <982627767@qq.com>
2020-04-29 18:56:59 +0800

    sas11

--- miniprogram/pages/appoint/appoint.js
+++ miniprogram/pages/appoint/appoint.js
@@ -1,32 +1,72 @@
 // miniprogram/pages/appoint/appoint.js\
 const citys = {
-  '浙江': ['杭州', '宁波', '温州', '嘉兴', '湖州'],
-  '福建': ['福州', '厦门', '莆田', '三明', '泉州']
+  '9时': ['00分', '30分'],
+  '10时': ['00分', '30分'],
+  '11时': ['00分', '30分'],
+  '14时': ['00分', '30分'],
+  '15时': ['00分', '30分'],
+  '16时': ['00分', '30分'],
+  '17时': ['00分', '30分']
 }
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
+  data: { 
+    xueyuan:'',
+    nianji:'',
+    message:'',
     value: '',
+    diseases:[
+      {num:'0',value:"感冒",checked:false},
+      {num:'1',value:"胃炎",checked:false},
+      {num:'2',value:"发烧",checked:false}
+    ],
     columns: [
       {
         values: Object.keys(citys),
         className: 'column1'
       },
       {
-        values: citys['浙江'],
+        values: citys['9时'],
         className: 'column2',
         defaultIndex: 2
       }
     ],
     show: false,
   },
+  //获取病类数据
+  radioChange(e){
+    console.log('单选框：',e.detail.value);
+  },
+  //获取学院数据
+  inputXueyuan(e){
+    this.setData({
+      xueyuan:e.detail
+    }),
+    console.log(e);
+  },
+  //获取年级信息
+  inputNianji(e){
+    this.setData({
+      nianji:e.detail
+    }),
+    console.log(e);
+  },
+  //获取病症信息
+  inputMsg(e){
+    this.setData({
+      message:e.detail
+    }),
+    console.log(e);
+  },
   onChange(event) {
     // event.detail 为当前输入的值
     console.log(event.detail);
   },
+
+
   change(event) {
     const { picker, value, index } = event.detail;
     console.log(event)
@@ -35,7 +75,6 @@ Page({
   showPopup() {
     this.setData({ show: true });
   },
-
   onClose() {
     this.setData({ show: false });
   },
@@ -44,7 +83,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
